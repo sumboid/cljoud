ae5ea97d23e73e4cc77d13ceca0e6ed0b8c75b71
neexee <neexee>
2014-12-25 22:44:06 +0600

    Node start script

--- examples/cljoud/example/node.clj
+++ examples/cljoud/example/node.clj
@@ -1,2 +1,5 @@
-()
-
+(ns cljoud.example.node
+  (:use [cljoud node]))
+(defn -main[& args]
+  (dotimes [i 4]
+        (.start (Thread. (start-node (+ 4000 i) "127.0.0.1:8000")))))
--- src/cljoud/node.clj
+++ src/cljoud/node.clj
@@ -1,6 +1,7 @@
 (ns cljoud.node
   (:use [cljoud serialization common])
-  (:use [cljoud tcp]))
+  (:use [cljoud tcp])
+  (:use [co.paralleluniverse.pulsar core actors]))
 
 (defn do-map[func-name func-code params]
   (create-ns 'user)
@@ -16,13 +17,12 @@
         params (deserialize (nth data 2))]
     (do-map func-name func-code params)))
 
-(defn start-node [host-port-str]
-  (let [host port] (host-port host-port-str)
-    (start-tcp-client[host port])))
-(defn -main [& args]
-  (let [node-soc (create-client-socket 8080)
-        nl (spawn-fiber node-listener nm nl-soc)]
+(defn start-node [port manager-addr]
+  (let [ [maddr mport] (host-port manager-addr)
+         node-soc (create-client-socket port)
+         wm (worker)
+         mc (spawn-fiber manager-client wm node-soc)
+         ml (spawn-fiber manager-listener wm node-soc)]
     (do
-      (join nm)
-      (join nl))))
+      (join ml))))
 
